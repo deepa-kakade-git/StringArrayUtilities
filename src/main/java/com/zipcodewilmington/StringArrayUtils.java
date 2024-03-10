4c688f2ea5e7cf68a67e049d59c5c468869d5c04
deepa-kakade-git <deep_303@yahoo.com>
2024-03-10 16:33:27 -0400

    finished

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -17,6 +20,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+
         return array[1];
     }
 
@@ -25,7 +29,9 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        int len = array.length;
+
+        return array[len - 1];
     }
 
     /**
@@ -33,7 +39,8 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 2];
     }
 
     /**
@@ -42,23 +49,52 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        //String str = value;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                return true;
+            }
+
+        }
+
         return false;
     }
+
 
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] arr = new String[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+            // for (int j =0 ; j< array.length; j++){
+
+            arr[j] = array[i];
+
+        }
+        return arr;
     }
+
 
     /**
      * @param array array of String objects
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            if (!array[start].equals(array[end])) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 
     /**
@@ -66,8 +102,26 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+//
+        boolean[] visited = new boolean[26]; // checks if each letter has been visited or not
+
+        int count = 0; // Count of distinct letters visited
+
+        for (String str : array) {
+            for (char c : str.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    int index = Character.toLowerCase(c) - 'a';
+                    if (!visited[index]) {
+                        visited[index] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count == 26;
     }
+
 
     /**
      * @param array array of String objects
@@ -75,7 +129,14 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int counter = 0;
+        for (String s : array) {
+            if (s.contains(value)) {
+                counter++;
+            }
+
+        }
+        return counter;
     }
 
     /**
@@ -84,7 +145,15 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (!Objects.equals(array[i], valueToRemove)) {
+                arr.add(array[i]);
+            }
+
+        }
+        return arr.toArray(new String[0]);
+
     }
 
     /**
@@ -92,16 +161,50 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        if (array.length == 0) {
+            return new String[0];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(array[0]);
+
+        for (int i = 1; i < array.length; i++) {
+            if (!array[i].equals(array[i - 1])) {
+                sb.append(" ").append(array[i]);
+            }
+        }
+
+        return sb.toString().split(" ");
     }
+
 
     /**
      * @param array array of chars
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        //declares a new arraylist called packedList to store consecutive duplicates
+        ArrayList<String> packedList = new ArrayList<>();
+        if (array.length == 0){
+            return new String[0]; //if array is empty this method returns empty array
+        }
+        StringBuilder currentStringBuilder = new StringBuilder(array[0]);
+        //above line declares currentStrigBuilder thats assigned the value of first index element of array
+
+        for (int i =1 ; i < array.length; i++){
+            if (array[i].equals(array[i -1])) {
+                currentStringBuilder.append(array[i]);
+            } else {
+                packedList.add(currentStringBuilder.toString());
+                currentStringBuilder = new StringBuilder(array[i]);
+            }
+
+        }
+        packedList.add(currentStringBuilder.toString());
+        //After the loop ends, it adds the string representation of the currentStringBuilder to the packedList.
+        return packedList.toArray(new String[0]);
     }
 
-
 }
+
+
